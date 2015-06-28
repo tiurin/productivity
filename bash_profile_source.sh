9bfdf5665162e5ad105067eb0612b7bf3b7f9828
Mykhailo Tiurin <mtyurin@gmail.com>
2015-06-28 13:31:01 +0200

    Add .bash_profile tweaks

---
+++ bash_profile_source.sh
@@ -0,0 +1,13 @@
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:$PATH
+
+source ~/workspaces/git/contrib/completion/git-prompt.sh
+source ~/workspaces/git/contrib/completion/git-completion.bash
+
+# Custom bash prompt via kirsle.net/wizards/ps1.html
+export GIT_PS1_SHOWDIRTYSTATE=1
+export PS1='\u@\h: \[$(tput bold)\]\w\[$(tput setaf 5)\]$(__git_ps1)\[$(tput sgr0)\]\n$ '
+alias ll='ls -l'
+alias la='ls -la'
+alias gits='git status'
+alias gitac='git add --all && git commit'
