234f18dec14bfb3721b69887d57e3eb3bb99e95c
Mykhailo Tiurin <mt@reviewpro.com>
2015-06-30 11:50:14 +0200

    Update $ sign color in command prompt

--- bash_profile_source.sh
+++ bash_profile_source.sh
@@ -6,7 +6,7 @@ source ~/workspaces/git/contrib/completion/git-completion.bash
 
 # Custom bash prompt via kirsle.net/wizards/ps1.html
 export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1='\u@\h: \[$(tput bold)\]\w\[$(tput setaf 5)\]$(__git_ps1)\[$(tput sgr0)\]\n$ '
+export PS1='\u@\h: \[$(tput bold)\]\w\[$(tput setaf 5)\]$(__git_ps1)\[$(tput setaf 1)\]\n$\[$(tput sgr0)\] '
 alias ll='ls -l'
 alias la='ls -la'
 alias gits='git status'
