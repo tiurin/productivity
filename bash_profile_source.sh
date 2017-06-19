64159b47a9dd0102b6b67a1fef7c8281c8c58805
Mykhailo Tiurin <mtyurin@gmail.com>
2017-06-19 23:21:26 +0200

    Set JAVA_HOME to Java 8 by default
    Now that Java 9 is available to play with
    it is useful to automatically go back to Java 8
    after playing.

--- bash_profile_source.sh
+++ bash_profile_source.sh
@@ -1,6 +1,9 @@
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 
+# To go back to Java 8 automatically after playing with Java 9
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+
 source ~/workspaces/git/contrib/completion/git-prompt.sh
 source ~/workspaces/git/contrib/completion/git-completion.bash
 
