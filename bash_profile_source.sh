9872675f98d28a6d4f3e35ee5b4193a31d2559b4
Misha Tiurin <misha.tiurin@skyscanner.net>
2019-05-02 11:22:14 +0200

    Set UTF-8 locale

--- bash_profile_source.sh
+++ bash_profile_source.sh
@@ -1,6 +1,6 @@
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
-export LANG=en
+export LANG=en_US.UTF-8
 export GPG_TTY=$(tty)
 # To go back to Java 8 automatically after playing with Java 9
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
