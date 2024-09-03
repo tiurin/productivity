56708eb9fcd2c5809178abc5e272a061d50628dd
Misha Tiurin <misha@tiurin.com>
2024-09-03 11:49:02 +0200

    Remove bitwarden from default apps

--- setup_brew.sh
+++ setup_brew.sh
@@ -8,7 +8,6 @@ brew install --quiet --cask iterm2
 brew install --quiet --cask font-jetbrains-mono
 
 brew install --quiet --cask 1password
-brew install --quiet --cask bitwarden
 
 brew install --quiet --cask google-chrome
 brew install --quiet --cask brave-browser
