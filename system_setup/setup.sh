3c929c87df0f472170e5ab7c06d73ca0d67131ca
Misha Tiurin <misha@tiurin.com>
2024-10-24 14:52:57 +0200

    Add JDK setup

--- system_setup/setup.sh
+++ system_setup/setup.sh
@@ -4,3 +4,4 @@ source ./setup_system.sh
 source ./setup_shell.sh
 source ./setup_brew.sh
 source ./setup_python.sh
+source ./setup_java.sh
---
+++ system_setup/setup_java.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+curl -s "https://get.sdkman.io" | bash
+source /Users/mt/.sdkman/bin/sdkman-init.sh
+
+sdk install java
