02a6b7dc81ef101daba0594155a71f2e13f26ec7
Misha Tiurin <misha@tiurin.com>
2024-10-26 19:45:52 +0200

    Set global python version

--- system_setup/setup_python.sh
+++ system_setup/setup_python.sh
@@ -1,9 +1,17 @@
 #!/bin/zsh
 
+#prepare pyenv
 brew install --quiet pyenv
 
-# from https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-brew install openssl readline sqlite3 xz zlib tcl-tk
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+source ~/.zshrc
 
+# from https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+brew install --quiet openssl readline sqlite3 xz zlib tcl-tk
+
+#install latest python 3 version and set it as global
 pyenv install 3
+pyenv latest 3 | xargs pyenv global
 
