226e829049bdfe3a955f9d8f0304b3cf54c35ecd
Misha Tiurin <misha@tiurin.com>
2024-10-29 21:12:20 +0100

    Add activate alias for python venvs

--- system_setup/setup_python.sh
+++ system_setup/setup_python.sh
@@ -6,6 +6,14 @@ brew install --quiet pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo '
+# alias to activate venv with a sensible default of .venv as virtual environment name.
+activate() {
+    local venv_name=${1:-".venv"}
+    source $venv_name/bin/activate
+}
+' >> ~/.zshrc
+
 source ~/.zshrc
 
 # from https://github.com/pyenv/pyenv/wiki#suggested-build-environment
