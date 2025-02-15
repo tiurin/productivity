845d8ec52ea876d58fdcf98291215126c3852dc5
Misha Tiurin <misha@tiurin.com>
2025-02-15 17:10:46 +0100

    Reorganize programs by type

--- system_setup/brew.sh
+++ system_setup/brew.sh
@@ -14,20 +14,18 @@ brew install --quiet htop
 brew install --quiet jq
 brew install --quiet tree
 brew install --quiet rename
-brew install --quiet localstack-cli
-brew install --quiet 1password-cli
 
 # install containers
 brew install --quiet lima
 brew install --quiet orbstack
+brew install --quiet --cask docker
+brew install --quiet localstack-cli
 
 # install apps
-brew install --quiet --cask docker 
 brew install --quiet --cask iterm2
 
-brew install --quiet --cask font-jetbrains-mono
-
 brew install --quiet --cask 1password
+brew install --quiet 1password-cli
 brew install --quiet --cask appcleaner
 
 brew install --quiet --cask google-chrome
@@ -35,6 +33,7 @@ brew install --quiet --cask brave-browser
 brew install --quiet --cask firefox
 
 brew install --quiet --cask jetbrains-toolbox
+brew install --quiet --cask font-jetbrains-mono
 brew install --quiet --cask visual-studio-code
 
 brew install --quiet --cask rectangle-pro
