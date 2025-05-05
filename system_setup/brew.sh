94fe3c4d1247b22860758ab304a17b2b0aa2c5d5
Misha Tiurin <misha@tiurin.com>
2025-05-05 17:44:45 +0200

    Install PyCharm directly instead of Jetbrains Toolbox app

--- system_setup/brew.sh
+++ system_setup/brew.sh
@@ -32,8 +32,8 @@ brew install --quiet --cask google-chrome
 brew install --quiet --cask brave-browser
 brew install --quiet --cask firefox
 
-brew install --quiet --cask jetbrains-toolbox
 brew install --quiet --cask font-jetbrains-mono
+brew install --quiet --cask pycharm
 brew install --quiet --cask visual-studio-code
 
 brew install --quiet --cask rectangle-pro
